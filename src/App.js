aceb34b98b627fd5b7f42803bb64bd65650b624f
Михаил Воевудский <michael.voevudskiy@gmail.com>
2025-01-20 15:15:22 +0200

    add found

--- src/App.js
+++ src/App.js
@@ -4,63 +4,62 @@ import React from 'react';
 
 import { useState, useEffect } from 'react';
 
-import Cat from './CardImg/Cat.png'
-import Chicken from './CardImg/Chicken.png'
-import Dog from './CardImg/Dog.png'
-import Goat from './CardImg/Goat.png'
-import Hamster from './CardImg/Hamster.png'
-import Parrot from './CardImg/Parrot.png'
-import Rooster from './CardImg/Rooster.png'
-import Turkey from './CardImg/Turkey.png'
+import Cat from './CardImg/Cat.png';
+import Chicken from './CardImg/Chicken.png';
+import Dog from './CardImg/Dog.png';
+import Goat from './CardImg/Goat.png';
+import Hamster from './CardImg/Hamster.png';
+import Parrot from './CardImg/Parrot.png';
+import Rooster from './CardImg/Rooster.png';
+import Turkey from './CardImg/Turkey.png';
+import Jellyfish from './CardImg/Jellyfish.png';
+import Octopus from './CardImg/Octopus.png';
+import Raccoon from './CardImg/Raccoon.png';
+import Rhinoceros from './CardImg/Rhinoceros.png';
+import Squirrel from './CardImg/Squirrel.png';
+import Stingray from './CardImg/Stingray.png';
+import Tiger from './CardImg/Tiger.png';
+import Whale from './CardImg/Whale.png';
 
-import Jellyfish from './CardImg/Jellyfish.png'
-import Octopus from './CardImg/Octopus.png'
-import Raccoon from './CardImg/Raccoon.png'
-import Rhinoceros from './CardImg/Rhinoceros.png'
-import Squirrel from './CardImg/Squirrel.png'
-import Stingray from './CardImg/Stingray.png'
-import Tiger from './CardImg/Tiger.png'
-import Whale from './CardImg/Whale.png'
+import sdCat from './CardSd/Cat.mp3';
+import sdChicken from './CardSd/Chicken.mp3';
+import sdDog from './CardSd/Dog.mp3';
+import sdGoat from './CardSd/Goat.mp3';
+import sdHamster from './CardSd/Hamster.mp3';
+import sdParrot from './CardSd/Parrot.mp3';
+import sdRooster from './CardSd/Rooster.mp3';
+import sdTurkey from './CardSd/Turkey.mp3';
+import sdJellyfish from './CardSd/Jellyfish.mp3';
+import sdOctopus from './CardSd/Octopus.mp3';
+import sdRaccoon from './CardSd/Raccoon.mp3';
+import sdRhinoceros from './CardSd/Rhinoceros.mp3';
+import sdSquirrel from './CardSd/Squirrel.mp3';
+import sdStingray from './CardSd/Stingray.mp3';
+import sdTiger from './CardSd/Tiger.mp3';
+import sdWhale from './CardSd/Whale.mp3';
 
+import sdSuccess from './CardSd/Success.wav'; // Добавляем звук для правильного ответа
+// import sdFail from './CardSd/Fail.wav'; // Добавляем звук для ошибки
 
-
-// let anim1 = [
-//   { id: 0, who: Cat, status: false},
-//   { id: 1, who: Chicken, status: false },
-//   { id: 2, who: Dog, status: false },
-//   { id: 3, who: Goat, status: false },
-//   { id: 4, who: Hamster, status: false },
-//   { id: 5, who: Parrot, status: false },
-//   { id: 6, who: Rooster, status: false },
-//   { id: 7, who: Turkey, status: false },
-//   { id: 8, who: Jellyfish, status: false },
-//   { id: 9, who: Octopus, status: false },
-//   { id: 10, who: Raccoon, status: false },
-//   { id: 11, who: Rhinoceros, status: false },
-//   { id: 12, who: Squirrel, status: false },
-//   { id: 13, who: Stingray, status: false },
-//   { id: 14, who: Tiger, status: false },
-//   { id: 15, who: Whale, status: false },
-// ]
 
 
 let anim1 = [
-  { id: 0, who: Cat, status: false, found: false },
-  { id: 1, who: Chicken, status: false, found: false  },
-  { id: 2, who: Dog, status: false, found: false  },
-  { id: 3, who: Goat, status: false, found: false  },
-  { id: 4, who: Hamster, status: false, found: false  },
-  { id: 5, who: Parrot, status: false, found: false  },
-  { id: 6, who: Rooster, status: false, found: false  },
-  { id: 7, who: Turkey, status: false, found: false  },
-  { id: 8, who: Jellyfish, status: false, found: false  },
-  { id: 9, who: Octopus, status: false, found: false  },
-  { id: 10, who: Raccoon, status: false, found: false  },
-  { id: 11, who: Rhinoceros, status: false, found: false  },
-  { id: 12, who: Squirrel, status: false, found: false  },
-  { id: 13, who: Stingray, status: false, found: false  },
-  { id: 14, who: Tiger, status: false, found: false  },
-  { id: 15, who: Whale, status: false, found: false  },
+  { id: 0, who: Cat, status: false, found: false, sound: sdCat },
+  { id: 1, who: Chicken, status: false, found: false, sound: sdChicken },
+  { id: 2, who: Dog, status: false, found: false, sound: sdDog },
+  { id: 3, who: Goat, status: false, found: false, sound: sdGoat },
+  { id: 4, who: Hamster, status: false, found: false, sound: sdHamster },
+  { id: 5, who: Parrot, status: false, found: false, sound: sdParrot },
+  { id: 6, who: Rooster, status: false, found: false, sound: sdRooster },
+  { id: 7, who: Turkey, status: false, found: false, sound: sdTurkey },
+  { id: 8, who: Jellyfish, status: false, found: false, sound: sdJellyfish },
+  { id: 9, who: Octopus, status: false, found: false, sound: sdOctopus },
+  { id: 10, who: Raccoon, status: false, found: false, sound: sdRaccoon },
+  { id: 11, who: Rhinoceros, status: false, found: false, sound: sdRhinoceros },
+  { id: 12, who: Squirrel, status: false, found: false, sound: sdSquirrel },
+  { id: 13, who: Stingray, status: false, found: false, sound: sdStingray },
+  { id: 14, who: Tiger, status: false, found: false, sound: sdTiger },
+  { id: 15, who: Whale, status: false, found: false, sound: sdWhale },
 ]
 
 
@@ -108,7 +107,7 @@ function App() {
 
     setList(updatedAnim1);
 
-    
+
   };
 
 
@@ -116,7 +115,8 @@ function App() {
     const animals = [...list];
 
     if (Select === id) {
-      
+
+
       for (let i = 0; i < animals.length; i++) {
         if (animals[i].id === id) {
           animals[i].found = true;
@@ -124,16 +124,24 @@ function App() {
       }
       setList(animals);
 
+      setTimeout(() => {
+        const audio = new Audio(sdSuccess);
+        audio.play();
+      }, 1000);
+
     }
 
-    else setSelect(id);
+    else {
+      setSelect(id);
 
+
+    }
   }
 
 
 
   let card = list.map((e, index) => (
-    <CardFlip key={index} index={index} id={e.id} who={e.who} status={e.status} found={e.found}
+    <CardFlip key={index} index={index} id={e.id} who={e.who} status={e.status} found={e.found} sound={e.sound}
       setList={setList} list={list} clickCount={clickCount} setClickCount={setClickCount}
       deactivateAllAnimals={deactivateAllAnimals} SelectedItems={SelectedItems}
       setSelect={setSelect} Select={Select} />
@@ -142,6 +150,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='resetgamebt' onClick={getRandomValues}> Restart</div>
         <div className="plate">{card}</div>
       </header>
     </div>
--- src/component/CardFlip.jsx
+++ src/component/CardFlip.jsx
@@ -1,32 +1,49 @@
 import styles from './CardFlip.module.css'
 import Frontcard from './../CardImg/Front.png'
+import React from 'react';
+
+
+import sdFail from './../CardSd/Fail.wav'; // Добавляем звук для ошибки
+// import sdSuccess from './../CardSd/Success.wav'; // Добавляем звук для правильного ответа
 
 const CardFlip = (props) => {
 
   const handleClick = () => {
 
-   
+    const audio = new Audio(props.sound);
+    audio.play();
+
+
     if (props.status === false) {
+
+
       // Копируем список и обновляем статус карточки
       const card = [...props.list];
 
-      if(props.clickCount === 1){
+      if (props.clickCount === 1) {
         setTimeout(() => {
           props.deactivateAllAnimals(); // Деактивируем все карточки
           props.setClickCount(0); // Сбрасываем счетчик кликов
           props.SelectedItems(null);  // Сбрасываем выбранные карточки
-        }, 500); 
+
+          const audio = new Audio(sdFail);
+          audio.play();
+
+        }, 1000);
+
+
+
       }
-      
+
       if (card[props.index]) {
-        card[props.index] = { ...card[props.index], status: !props.status}; // Используем новое состояние для обновления
+        card[props.index] = { ...card[props.index], status: !props.status }; // Используем новое состояние для обновления
         props.setList(card); // Передаем обновленный список
       }
-      
+
       else {
         console.error(`Card with index ${props.index} does not exist.`);
       }
-      
+
       props.setClickCount(props.clickCount + 1); // Увеличиваем счетчик кликов
       props.SelectedItems(props.id) // Передаем id выбранной карточки
     }
@@ -34,7 +51,7 @@ const CardFlip = (props) => {
 
   return (
     <div className={styles.cardContainer} onClick={handleClick}>
-      <div className={`${styles.card} ${props.status || props.found? styles.flipped : ''}`}>
+      <div className={`${styles.card} ${props.status || props.found ? styles.flipped : ''}`}>
         <div className={styles.cardFace + ' ' + styles.cardFront}>
           <img src={Frontcard} alt="Front" />
         </div>
